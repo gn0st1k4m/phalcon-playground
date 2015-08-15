addaa2c63dc1ce4c2eeed730fd413f4aa68c41f3
mamuz <mamuz.de@gmail.com>
2015-08-15 21:16:15 +0200

    add scripts to composer

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 chdir(dirname(__DIR__));
 
 include 'vendor/autoload.php';
 
-$env = getenv('APPLICATION_ENV') ?: 'production';
+$env = trim(file_get_contents('./ENV'));
 $configCache = './data/cache/config.php';
 
 Phpg\Application\Bootstrap::init($env, $configCache)->runApplicationOn($_SERVER);
